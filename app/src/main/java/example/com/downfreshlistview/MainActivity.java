fc6492b54f74999b0f61eafdc0cd852cb1829383
HsHWB <Hswbinforgithub@gmail.com>
2015-02-17 14:17:21 +0800

    base framework
    上拉刷新

---
+++ app/src/main/java/example/com/downfreshlistview/MainActivity.java
@@ -0,0 +1,66 @@
+package example.com.downfreshlistview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import example.com.downfreshlinearlayout.MyLinearLayout;
+
+
+public class MainActivity extends Activity {
+
+    MyLinearLayout refreshableView;
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    String[] items = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        refreshableView = (MyLinearLayout) findViewById(R.id.mylayout);
+        listView = (ListView) findViewById(R.id.listview);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(adapter);
+        refreshableView.setOnRefreshListener(new MyLinearLayout.PullToRefreshListener() {
+            @Override
+            public void onRefresh() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                refreshableView.finishRefreshing();
+                }
+            },0);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
